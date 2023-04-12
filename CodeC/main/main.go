4211ceb13ec0919825e39090eaeb1fa9b5f3e4e6
TTtian1998 <1210059544@qq.com>
2023-04-12 23:41:38 +0800

    adjust the demo example

--- CodeC/main/main.go
+++ CodeC/main/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"rpc_go"
@@ -10,6 +9,10 @@ import (
 )
 
 func startServer(addr chan string) {
+	var foo Foo
+	if err := rpc_go.Register(&foo); err != nil {
+		log.Fatal("register error:", err)
+	}
 	// pick a free port
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
@@ -25,9 +28,7 @@ func main() {
 	addr := make(chan string)
 	go startServer(addr)
 	client, _ := rpc_go.Dial("tcp", <-addr)
-	defer func() {
-		_ = client.Close()
-	}()
+	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
 	// send request & receive response
@@ -36,13 +37,22 @@ func main() {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			args := fmt.Sprintf("rpc req %d", i)
-			var reply string
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
 			if err := client.Call("Foo.Sum", args, &reply); err != nil {
 				log.Fatal("call Foo.Sum error:", err)
 			}
-			log.Println("reply:", reply)
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
 		}(i)
 	}
 	wg.Wait()
+}
+
+type Foo int
+
+type Args struct{ Num1, Num2 int }
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
 }
